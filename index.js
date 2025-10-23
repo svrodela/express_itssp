0303750ceaa42f4b7ffb08bf6892f82b6140fa58
svrodela <svrodela@gmail.com>
2025-10-23 11:59:25 -0600

    conexión a la bd

--- index.js
+++ index.js
@@ -2,29 +2,67 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.use(express.json());
+
 //motor de vistas
 app.set('view engine', 'ejs');
 
-app.get('/i', (req, res) => {
-  res.render('inicio', {
-    titulo: 'Página de Inicio',
-    mensaje: 'Bienvenido a nuestra tienda en línea'
-  });
+//express
+const admin = require("firebase-admin");
+const cors = require("cors");
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+// credenciales
+const serviceAccount = require("./firebase_key.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+const db = admin.firestore();// Rutas de la API de productos
+ 
+
+
+app.get("/productos", async (req, res) => {
+  try {
+    const items = await db.collection("productos").get();
+    const productos = items.docs.map(doc => {
+      const data = doc.data();
+      return {
+        id: doc.id,
+        nombre: data.nombre,
+        descripcion: data.descripcion,
+        precio: data.precio,
+        imagen: data.imagen
+      };
+    });
+    res.render('inicio', { productos });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-app.get('/',(req,res)=>{  
-    res.render('index', {
-    titulo: 'Productos',
-    mensaje: 'Productos disponibles en la tienda',
-    productos: [
-      { nombre: 'Producto 1', precio: 10.99 },
-      { nombre: 'Producto 2', precio: 19.99 },
-      { nombre: 'Producto 3', precio: 5.49 },
-    ]
-  });
- });
+app.get('/productos/add', (req, res) => {
+res.render('form', { producto: null, nombre: 'Crear Producto' });
+});
+app.post('/productos', async (req, res) => {
+try {
+    const { nombre, precio, descripcion, imagen } = req.body;
+    const nuevo = {
+    nombre: nombre || '',
+    precio: parseFloat(precio) || 0,
+    descripcion,
+    imagen: imagen || ''
+};
+await db.collection('productos').add(nuevo);
+res.redirect('/productos');
+} catch (err) {
+    console.error(err);
+    res.status(500).send('Error al crear producto');
+}
+});
 
- app.get('/inicio', (req, res) => {
+
+
+ app.get('/productos1', (req, res) => {
   const productos = [
     {
       nombre: 'Audífonos Bluetooth',
